c8d8f02116eab745b28224aa9cc935f9ec526135
Fatlum <socials.fatlum@gmail.com>
2025-03-05 13:34:16 +0100

    changes

--- src/data/settings.ts
+++
@@ -1,6 +0,0 @@
-export const settings = {
-  site: 'https://LUMI-Game-Studios.github.io',
-  name: 'Little Sticks',
-  title: 'Cannonball by Little Sticks | A splash page template',
-  description: 'This is a simple splash page template built with Astro by Little Sticks',
-}
